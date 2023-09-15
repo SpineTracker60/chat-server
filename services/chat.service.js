0e417334f56733b421acc66f31571d983f91440a
jeongjiwon <jeewonre@hufs.ac.kr>
2023-09-15 16:14:15 +0900

    Feat : 챗봇 서버 연결

--- services/chat.service.js
+++ services/chat.service.js
@@ -1,5 +1,8 @@
 import ChatModel from "../models/chat";
 import roomService from "../services/room.service";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 exports.create = async (io, chatDTO) => {
   const room = await roomService.findByMemberId(chatDTO.memberId);
@@ -13,4 +16,24 @@ exports.create = async (io, chatDTO) => {
   const newChat = await chat.save();
   io.of("/room").to(roomId).emit("chat", chat);
   return { success: true, newChat };
+};
+
+exports.requestChatBot = async (io, chatData, memberInfo, memberPosture) => {
+  try {
+    const chatbotResults = await axios.post(process.env.CHATBOT_HOST, {
+      id: chatData.sender_member,
+      question: chatData.body,
+      age: memberInfo.age,
+      gender: memberInfo.gender,
+      job: memberInfo.job,
+      turtle_neck: memberPosture.turtle_neck,
+      sleepiness: memberPosture.sleepiness,
+      stooped_position: memberPosture.stooped_position,
+    });
+    io.of("/room").to(chatData.room).emit("chat", chatbotResults.data);
+    return { success: true, newChat: chatbotResults.data };
+  } catch (err) {
+    console.log(err);
+    return { success: false, err };
+  }
 };
